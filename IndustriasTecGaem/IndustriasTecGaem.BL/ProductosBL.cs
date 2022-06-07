05fe46ba8d460e179aa0c410887cef8aede73eb7
Gelsyn <gelsyn.medina@unah.hn>
2022-06-06 20:40:31 -0600

    creamos una arquitectura basica y creamos 2 clases

---
+++ IndustriasTecGaem/IndustriasTecGaem.BL/ProductosBL.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndustriasTecGaem.BL
+{
+    public class ProductosBL
+    {
+        public List<Producto> ObtenerProductos()
+        {
+            return listaProductos;
+        }
+    }
+}
